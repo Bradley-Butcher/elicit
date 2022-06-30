cfdddf4c6876532cc2eca6bfb4fbe5c1217950eb
Bradley-Butcher <bradleydpbutcher@gmail.com>
2022-06-30 13:59:48 +0100

    install fix

--- elicit/generic_labelling_functions/__init__.py
+++ elicit/generic_labelling_functions/__init__.py
@@ -1,4 +1,9 @@
-from .similarity_transformer import SimilarityLabellingFunction
+import warnings
 from .keyword_search import KeywordMatchLF
-from .nli_transformer import NLILabellingFunction
-from .semantic_search import SemanticSearchLF
+try:
+    from .similarity_transformer import SimilarityLabellingFunction
+    from .nli_transformer import NLILabellingFunction
+    from .semantic_search import SemanticSearchLF
+except:
+    warnings.warn(
+        "Tranformer libraries not installed. Transformer labelling functions will not be available.")
